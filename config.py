9c3573f5788d231d8b76932effc4e4d1fc1f0e44
FHuntet05 <129078356+FHuntet05@users.noreply.github.com>
2023-09-21 14:59:03 -0400

    config.py
    Signed-off-by: FHuntet05 <129078356+FHuntet05@users.noreply.github.com>

--- config.py
+++ config.py
@@ -2,21 +2,21 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "27026389")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "158b014213c39d3b342a8792e495a5dc")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6139931468:AAEInK7S9neJ5Z_IlJx3bWE8s7JWDvamFfU") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "WebXBots") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "fsremote") 
 
-DB_NAME = os.environ.get("DB_NAME","")     
+DB_NAME = os.environ.get("DB_NAME","Cluster0")     
 
-DB_URL = os.environ.get("DB_URL","")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://fefthunter:Fefthunter230405@cluster0.9iukqrh.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "https://graph.org/Rename-Bot-01-15")
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/5fc119c05326d2595965f.jpg")
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
